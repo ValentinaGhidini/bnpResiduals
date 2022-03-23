8a42133fd4c742854de53bd2b146761dfe77b20b
ValentinaGhidini <58432585+ValentinaGhidini@users.noreply.github.com>
2022-03-23 09:46:58 +0100

    Add files via upload

--- Parametricmodel.R
+++ Parametricmodel.R
@@ -1,105 +1,178 @@
+library(orthopolynom)
+library(MASS)
+library(invgamma)
 # Model Y = Xbeta + epsi ---> Y | beta, sigma2 ~ N(Xbeta, sigma2) 
 # epsi | SIGMA2 ~ N(0, sigma2)
 # beta | beta0 ~ N(beta0, sigma2b)
 # beta0 ~ N(0, sigma20)
 # sigma2 ~ IG(a, b)
 
+######### NEW MODEL ##################
+
+posterior_sigma2 <- function(c0, d0, X, Y, beta, groups){
+  N <- nrow(X)
+  new_shape <- c0 + N/2
+  residuals <- rep(0, N)
+  L <- length(unique(groups))
+  for (l in 1:L){
+    residuals[groups==l] <- Y[groups==l] - X[groups==l,] %*% beta[,l]
+  }
+  new_scale <- d0 + .5*sum(residuals^2)
+  sigma2_post <- rinvgamma(1, shape=new_shape, scale = new_scale)
+  return(sigma2_post)
+}
 
 
-
-sample_posterior_beta0 <- function(beta, sigma2beta, sigma20){
-   k <- length(beta)
-   beta0_post <- rnorm(n = 1, mean = mean(beta)*(sigma2beta*sigma20*k)/(k*sigma20+sigma2beta),
-                       sd= sqrt((sigma20*sigma2beta)/(k*sigma20+sigma2beta)))
+sample_posterior_beta0 <- function(beta, b0, B, B0){
+  if (is.null(dim(b0))){b0 <- matrix(b0, ncol=1)}
+  if (is.null(dim(beta))){beta <- matrix(beta, ncol=1)}
+  mean_vector <- apply(beta, 1, mean)
+  L <- ncol(beta)
+  k <- length(beta)
+  new_mean <- solve(L*solve(B) + solve(B0))%*%(L*solve(B)%*%mean_vector+solve(B0)%*%b0)
+  new_var <- solve(solve(B) + solve(B0))
+  beta0_post <- mvrnorm(1, mu= new_mean, Sigma = new_var)
   return(beta0_post)
 }
 
 
+posterior_beta <- function(X, Y, sigma2, B, beta0){
+  if (is.null(dim(b0))){b0 <- matrix(b0, ncol=1)}
+  if (is.null(dim(Y))){ Y <- matrix(Y, ncol=1)}
 
-loglik <- function(Y, sigma2, beta, X){
-  return(sum(dnorm(Y, X%*%beta, sd = sqrt(sigma2), log = T)))
-}
-
-posterior_beta <- function(X, Y, sigma2, sigma2beta, beta0){
-  k = ncol(X)
-  beta0 = rep(beta0, k)
-  B = diag(rep(sigma2beta, k))
   Bp = t(X)%*%X/sigma2+solve(B)
   bp = t(X)%*%Y/sigma2 + solve(B)%*%beta0
   post <- mvrnorm(1, mu = solve(Bp)%*% bp ,  Sigma= solve(Bp))
   return(post)
 }
 
-
-posterior_sigma2 <- function(c0, d0, X, Y, beta){
-  N <- nrow(X)
-  new_shape <- c0 + N/2
-  new_scale <- d0 + .5*sum((Y-X%*%beta)^2)
-  sigma2_post <- rinvgamma(1, shape=new_shape, scale = new_scale)
-  return(sigma2_post)
+loglik <- function(Y, sigma2, beta, X, groups){
+  L <- length(unique(groups))
+  llik <- 0
+  for (l in 1:L){
+    llik <- llik + sum(dnorm(Y[groups==l], X[groups==l,]%*%beta[,l], sd = sqrt(sigma2), log = T))
+  }
+  return(llik)
 }
-#### DATA ###
-n <- 100
-p <- 10
-beta_true <- rnorm(n=p, beta0, sigma20)#sample(1:10, replace=T, size=p)
 
-X = matrix(rnorm(n*p), ncol=p, nrow=n)
-Y = X%*% beta_true + rnorm(n)
-
+######## SIMULATION ######
 
 #### HYPERPARS ####
-sigma20 <- 1
-beta0 <- 0
-sigma2beta <- 1
+p <- 5
+# beta0 ~ N(b0, B0)
+b0 <- rep(0, p)
+B0 <- matrix(0.1, ncol=p, nrow=p)
+diag(B0) <- 1
+
+
+# beta|beta0 ~ N(beta0, B)
+B <- matrix(0, ncol=p, nrow=p)
+diag(B) <- 1
+
+# sigma2 ~ IG(c0, d0)
 c0 <- 1
 d0 <- 1
+
 
 ### INITIALIZATION ###
 sigma2 <- 1
 
-### GIBBS SAMPLER
+#### DATA ###
+n <- 100
 
-GS <- function(X, Y, beta, beta0, sigma2, sigma2beta, c0, d0, iters){
-  N <- nrow(X)
-  beta0_post <- c()
-  beta_post <- matrix(0, ncol=iters, nrow = p)
-  sigma2_post <- c()#matrix(0, ncol=iters, nrow = N)
-  
-  for (it in 1:iters){
-    beta0 <- sample_posterior_beta0(beta, sigma2beta, sigma20)
-    beta <- posterior_beta(X, Y, sigma2, sigma2beta, beta0)
-    sigma2 <- posterior_sigma2(c0, d0, X, Y, beta)
-    
-    beta0_post <- c(beta0_post, beta0)
-    beta_post[,it] <- beta
-    sigma2_post <- c(sigma2_post, sigma2)
-  }
- 
- return(list(beta0_post = beta0_post, beta_post = beta_post, sigma2_post = sigma2_post))  
+
+GRUPPI = 4
+
+beta0 <- mvrnorm(1, b0, B0)
+beta_true <- matrix(0, nrow=p, ncol=GRUPPI)#sample(1:10, replace=T, size=p)
+for (g in 1:GRUPPI) {
+  beta_true[,g]<- mvrnorm(1, beta0, B)
+}
+
+X = matrix(rnorm(n*p), ncol=p, nrow=n)
+groups <- sample (1:GRUPPI, size=n, replace=T)
+
+Y <- rep(0,n)
+for (g in 1:GRUPPI) {
+  Y[groups==g] = X[groups==g,]%*% beta_true[,g] + rnorm(sum(groups==g))
 }
 
 
 
-final <- GS(X, Y, beta_true, beta0, sigma2, sigma2beta, c0, d0, iters=10000)
+### GIBBS SAMPLER
+GS_new <- function(X, Y, beta0, B0,  B, c0, d0, sigma2, iters, groups){
+  # L = number of groups
+  # gruppo = gruppo di appartenenza
+  if (is.null(dim(Y))){ Y <- matrix(Y, ncol=1)}
+  N <- nrow(X)
+  beta0_post <- matrix(0, ncol=iters, nrow = p)
+  beta_post_full <- list()#matrix(0, ncol=iters, nrow = p)
+  sigma2_post <- c()#matrix(0, ncol=iters, nrow = N)
+  L <- length(unique(groups))
+  
+  for (it in 1:iters){
+    beta_post <- matrix(0, ncol=L, nrow = p)
+   
+    # sample betas for each group
+    for (l in 1:L){
+    
+      beta_post[,l] <- posterior_beta(X[groups==l, ], Y[groups==l], sigma2, B, beta0)
+      
+    }
+    beta0 <- sample_posterior_beta0(beta_post, b0, B, B0)
+    sigma2 <- posterior_sigma2(c0, d0, X, Y, beta_post, groups )
+    
+    beta0_post[,it] <- beta0
+    beta_post_full[[it]] <- beta_post
+    sigma2_post <- c(sigma2_post, sigma2)
+  }
+  
+  return(list(beta0_post = beta0_post, beta_post_full = beta_post_full, sigma2_post = sigma2_post))  
+}
 
-final$beta0_post
-beta_post <- apply(final$beta_post[,5000:10000], 1, mean)
+
+final<- GS_new(X, Y, beta0, B0,  B, c0, d0, sigma2, iters=10000, groups=groups)
+
+
+final$beta_post_full[[10000]]
+beta_true
+
+final_estimates_MAP <- matrix(0, nrow=p, ncol=GRUPPI)
+for (el in final$beta_post_full){
+  for (i in 1:nrow(el)){
+    for (j in 1:ncol(el)){
+      final_estimates_MAP[i, j] <- final_estimates_MAP[i, j] + el[i,j]
+    }
+  }
+}
+final_estimates_MAP <- final_estimates_MAP/10000
+final_estimates_MAP
+beta_true
+#beta_post <- apply(final$beta_post[,5000:10000], 1, mean)
+
+# Traceplot
 trace <- c()
 iters = 10000
 for (it in 1:iters){
-  tt <- loglik(Y, X = X , beta = final$beta_post[,it], sigma2 = final$sigma2_post[it])
+  tt <- loglik(Y, X = X , beta = final$beta_post_full[[it]], sigma2 = final$sigma2_post[it], groups=groups)
   trace <- c(trace, tt)
 }
 
-plot(trace, type="l")
-sum(Y-X%*%beta_post)/n
-plot(c(Y), c(X%*%beta_post))
-abline(a=1,b=1,col=2)
+plot(trace, type="l", main="Traceplot")
 
-beta_true
-beta_post
+# predictions
+preds <- rep(0, n)
 
-plot(density(Y-X%*%beta_post))
+for (l in 1:GRUPPI){
+  preds[groups==l] <-  X[groups==l,]%*%final_estimates_MAP[,l]
+}
+
+# Residuals
+plot(c(Y), preds, pch=19, main="Residuals")
+abline(a=0,b=1,col=2)
+
+# Density of residuals
+plot(density(Y-preds), main="Density of Residuals")
 lines(seq(-3,3, length=100), dnorm(seq(-3,3, length=100)), col=2)
 
              
